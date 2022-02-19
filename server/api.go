d713619f03e778f3d91083f8f454985724d5cc67
Teilat <kot.sergej2012@yandex.ru>
2022-02-19 23:50:50 +0300

    fix project structure

--- server/api.go
+++ server/api.go
@@ -14,7 +14,7 @@ func Init() {
 	router := gin.Default()
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
-	//router.Use(gin.BasicAuth()) map[string]string with login/pass
+	//router.Use(gin.BasicAuth()) //map[string]string with login/pass
 
 	ver1 := router.Group("/api/v1")
 	{
