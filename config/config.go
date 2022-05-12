68904605a5cab3e8e2a9cdd1956b8a0c68aa8c6c
Teilat <kot.sergej2012@yandex.ru>
2022-05-12 14:39:23 +0300

    fix

--- config/config.go
+++ config/config.go
@@ -7,13 +7,7 @@ import (
 
 type Configuration struct {
 	Sql Sql `yaml:"Sql"`
-	jwt Jwt `yaml:"Jwt"`
 	Api Api `yaml:"Api"`
-}
-
-type Jwt struct {
-	Usage     bool `yaml:"Enable"`
-	ExpiresAt int  `yaml:"ExpiresAt"`
 }
 
 type Api struct {
